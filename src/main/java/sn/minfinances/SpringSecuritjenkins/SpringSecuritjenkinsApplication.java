71d94907620377523db8e744adc7ef3ffdad132c
NeimaOsman <skyoussoufisidk@groupeisi.com>
2023-02-11 05:42:58 +0100

    first commit

---
+++ src/main/java/sn/minfinances/SpringSecuritjenkins/SpringSecuritjenkinsApplication.java
@@ -0,0 +1,13 @@
+package sn.minfinances.SpringSecuritjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/minfinances/SpringSecuritjenkins/SpringSecuritjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.minfinances.SpringSecuritjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
